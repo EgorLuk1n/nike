3bbd6e86b0e31563555e8fa3beb6467e72be40af
EgorLuk1n <151617796+EgorLuk1n@users.noreply.github.com>
2024-05-20 18:05:28 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => {
+  <main className="relative">
+    Nav
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      Hero
+      <section className="padding">Popular</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding">Service</section>
+      <section className="padding">SpecialOffer</section>
+      </section>
+  </main>;
+};
+
+export default App;
